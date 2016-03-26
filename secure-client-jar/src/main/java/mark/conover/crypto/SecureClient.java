e55375f75655058a125588d695078830606f3756
Mark Conover <conoverm157@gmail.com>
2016-03-25 21:18:29 -0400

    Added initial client/server functionality.

--- secure-client-jar/src/main/java/mark/conover/crypto/SecureClient.java
+++ secure-client-jar/src/main/java/mark/conover/crypto/SecureClient.java
@@ -1,9 +1,154 @@
 package mark.conover.crypto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class SecureClient {
-	
-	public static void main(String[] args) {
+
+	private String cookies;
+	private HttpClient client = HttpClientBuilder.create().build();
+	private static final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+		String url = "https://accounts.google.com/ServiceLoginAuth";
+		String gmail = "https://mail.google.com/mail/";
+
+		// make sure cookies is turn on
+		CookieHandler.setDefault(new CookieManager());
+
+		SecureClient http = new SecureClient();
+
+		String page = http.GetPageContent(url);
+
+		List<NameValuePair> postParams = http.getFormParams(page, "username",
+				"password");
+
+		http.sendPost(url, postParams);
+
+		String result = http.GetPageContent(gmail);
+		System.out.println(result);
+
+		System.out.println("Done");
+	}
+
+	private void sendPost(String url, List<NameValuePair> postParams) 
+			throws ClientProtocolException, IOException {
 		
+		HttpPost post = new HttpPost(url);
+
+		// add header
+		post.setHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
+		urlParameters.add(new BasicNameValuePair("cn", ""));
+		urlParameters.add(new BasicNameValuePair("locale", ""));
+		urlParameters.add(new BasicNameValuePair("caller", ""));
+		urlParameters.add(new BasicNameValuePair("num", "12345"));
+
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+		HttpResponse response = client.execute(post);
+		System.out.println("Response Code : "
+				+ response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+	}
+
+	private String GetPageContent(String url) throws Exception {
+
+		HttpGet request = new HttpGet(url);
+
+		request.setHeader("User-Agent", USER_AGENT);
+		request.setHeader("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		request.setHeader("Accept-Language", "en-US,en;q=0.5");
+
+		HttpResponse response = client.execute(request);
+		int responseCode = response.getStatusLine().getStatusCode();
+
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		// set cookies
+		setCookies(response.getFirstHeader("Set-Cookie") == null ? ""
+				: response.getFirstHeader("Set-Cookie").toString());
+
+		return result.toString();
+
+	}
+
+	public List<NameValuePair> getFormParams(String html, String username,
+			String password) throws UnsupportedEncodingException {
+
+		System.out.println("Extracting form's data...");
+
+		Document doc = Jsoup.parse(html);
+
+		// Google form id
+		Element loginform = doc.getElementById("gaia_loginform");
+		Elements inputElements = loginform.getElementsByTag("input");
+
+		List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+
+		for (Element inputElement : inputElements) {
+			String key = inputElement.attr("name");
+			String value = inputElement.attr("value");
+
+			if (key.equals("Email"))
+				value = username;
+			else if (key.equals("Passwd"))
+				value = password;
+
+			paramList.add(new BasicNameValuePair(key, value));
+
+		}
+
+		return paramList;
+	}
+
+	public String getCookies() {
+		return cookies;
+	}
+
+	public void setCookies(String cookies) {
+		this.cookies = cookies;
 	}
 
 }
--- secure-server-war/src/main/java/mark/conover/crypto/servlets/SecureServerServlet.java
+++ secure-server-war/src/main/java/mark/conover/crypto/servlets/SecureServerServlet.java
@@ -1,18 +1,27 @@
 package mark.conover.crypto.servlets;
 
 import java.io.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SecureServerServlet extends HttpServlet {
+	private static final long serialVersionUID = -7682825864889092242L;
 
 	private String message = null;
+	
+	private static final Logger LOG = LoggerFactory.getLogger(
+		SecureServerServlet.class);
 
 	public void init() throws ServletException {
 		// Do required initialization
 		message = "Hello World";
 	}
 
+	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// Set response content type
@@ -20,9 +29,17 @@ public class SecureServerServlet extends HttpServlet {
 
 		// Actual logic goes here.
 		PrintWriter out = response.getWriter();
-		out.println("<h1>" + message + "</h1>");
+		out.println("<h1>" + message + "</h1>"); 
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		
-		DHKeyAgreement2 
+		LOG.debug("doPost() method has been called.");
+		String publicKey = req.getParameter("publickey");
+		LOG.debug("publicKey is: " + publicKey);	
+		
 	}
 
 	public void destroy() {
